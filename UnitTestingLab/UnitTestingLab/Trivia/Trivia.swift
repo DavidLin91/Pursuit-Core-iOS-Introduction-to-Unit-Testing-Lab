d9b4082f5fb0e89f996138b86dd108196a8ca460
David <lin.david985@gmail.com>
2019-12-08 13:31:07 -0500

    parked trivia json data

--- UnitTestingLab/UnitTestingLab/Trivia/Trivia.swift
+++ UnitTestingLab/UnitTestingLab/Trivia/Trivia.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+struct Trivia: Codable {
+    let results: [TriviaData]
+}
+
+
+struct TriviaData: Codable {
+    let category: String
+    let question: String
+    let correctAnswer: String
+    let incorrectAnswer: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case category
+        case question
+        case correctAnswer = "correct_answer"
+        case incorrectAnswer = "incorrect_answer"
+    }
+}
+
+extension TriviaData {
+    static func getTrivia() -> [TriviaData] {
+    var trivia = [TriviaData]()
+    
+    guard let fileURL = Bundle.main.url(forResource: "trivia", withExtension: "json") else {
+    fatalError("")
+    }
+    
+    do {
+    let data = try Data(contentsOf: fileURL)
+    let triviaData = try JSONDecoder().decode(Trivia.self, from: data)
+    trivia = triviaData.results
+    } catch {
+        fatalError("")
+        }
+     return trivia
+    }
+}
--- UnitTestingLab/UnitTestingLab/Trivia/TriviaViewController.swift
+++ UnitTestingLab/UnitTestingLab/Trivia/TriviaViewController.swift
@@ -6,25 +6,22 @@
 //  Copyright © 2019 David Lin (Passion Proj). All rights reserved.
 //
 
+
+// VC: Questions
+// DVC: options for questions - right answer background green / wrong answer background red
+// disable other buttons
+
+
 import UIKit
 
 class TriviaViewController: UIViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
